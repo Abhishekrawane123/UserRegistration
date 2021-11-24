8bed4551761e1d14128b5dbecc45b9b6a1c68a54
pknowledge <abhishekrawane1195@gmail.com>
2021-11-24 07:46:23 +0530

    Modified : different email IDs from a file to check valid or not

--- src/com/bridgelabz/UserRegistration/Main.java
+++ src/com/bridgelabz/UserRegistration/Main.java
@@ -1,16 +1,18 @@
 package com.bridgelabz.UserRegistration;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		PatternCheck check = new PatternCheck();
 //		check.firstNamePattern();
 //		check.lastNamePattern();
 //		check.emailIdPattern();
 //		check.phoneNumber();
-		check.passwordPattern();
-		
+//		check.passwordPattern();
+		check.sampleEmailsChecking();;
 		check.printResult();
 	}
 }
--- src/com/bridgelabz/UserRegistration/PatternCheck.java
+++ src/com/bridgelabz/UserRegistration/PatternCheck.java
@@ -1,11 +1,20 @@
 package com.bridgelabz.UserRegistration;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class PatternCheck {
 
 	UserRegistration registration = new UserRegistration();
+
 	public void firstNamePattern() {
 
 		String pattern = "^[A-Z]{1}[a-z]{2,}";
@@ -17,8 +26,7 @@ public class PatternCheck {
 			if (Pattern.matches(pattern, fName)) {
 				registration.setFirstName(fName);
 				break;
-			}
-			else
+			} else
 				System.out.println("enter valid first name as -->Xyz...");
 		}
 	}
@@ -33,19 +41,18 @@ public class PatternCheck {
 			if (Pattern.matches(pattern, lName)) {
 				registration.setLastName(lName);
 				break;
-			}
-			else
+			} else
 				System.out.println("enter valid last name as -->Xyz...");
 		}
 	}
 
 	public void printResult() {
 		System.out.println(registration);
-		
+
 	}
 
 	public void emailIdPattern() {
-		
+
 		String pattern = "^[a-zA-Z0-9]+([-_+.a-zA-Z0-9])*@[A-Za-z]+.[A-Za-z]+(.[A-Za-z]{2})*$";
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("enterEmail ID : ");
@@ -54,8 +61,7 @@ public class PatternCheck {
 			if (Pattern.matches(pattern, email)) {
 				registration.setLastName(email);
 				break;
-			}
-			else
+			} else
 				System.out.println("enter valid email as ");
 		}
 	}
@@ -70,25 +76,48 @@ public class PatternCheck {
 			if (Pattern.matches(pattern, phoneNumber)) {
 				registration.setPhoneNumber(phoneNumber);
 				break;
-			}
-			else
+			} else
 				System.out.println("enter valid phone number ");
 		}
 	}
 
 	public void passwordPattern() {
-		
+
 		String pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,}$";
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("enter password with min 8 characters at least one upper case, one digit and one special characters : ");
+		System.out.println(
+				"enter password with min 8 characters at least one upper case, one digit and one special characters : ");
 		while (true) {
 			String password = scanner.nextLine();
 			if (Pattern.matches(pattern, password)) {
 				registration.setPassword(password);
 				break;
-			}
-			else
+			} else
 				System.out.println("enter valide password ");
-		}	
+		}
+	}
+
+	public void sampleEmailsChecking() throws IOException {
+
+		String pattern = "^[a-zA-Z0-9]+([-_+.]?[a-zA-Z0-9])*@[A-Za-z0-9]{1,}.[A-Za-z]{2,}(.[A-Za-z]{2,})*$";
+		String filePath = "C:\\Users\\ABHISHEK RAWANE\\eclipse-workspace\\UserRegistration\\src\\com\\bridgelabz\\UserRegistration\\differentEmails.txt";
+		List<String> lines = new ArrayList<String>();
+		Path path = Paths.get(filePath);
+		lines = Files.readAllLines(path);
+
+		BufferedWriter writer; 
+		writer = new BufferedWriter(new FileWriter("C:\\Users\\ABHISHEK RAWANE\\eclipse-workspace\\UserRegistration\\src\\com\\bridgelabz\\UserRegistration\\differentEmailsResults.txt"));
+		
+		
+		for (String line : lines) {
+			if (Pattern.matches(pattern, line)) {
+				System.out.println("EMAIL ID --> " + line + " --> VALIDE EMAIL");
+				writer.write("EMAIL ID --> " + line + " --> VALIDE EMAIL \n");
+			} else {
+				System.out.println("EMAIL ID --> " + line + " --> INVALIDE EMAIL");
+				writer.write("EMAIL ID --> " + line + " --> INVALIDE EMAIL \n");
+			}
+		}
+		writer.close();
 	}
 }
